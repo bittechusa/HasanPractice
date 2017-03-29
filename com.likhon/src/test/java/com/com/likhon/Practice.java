bf717ec93beda8352a56f43cac34d7fea42de0fd
mushfique <mush_bellicose@hotmail.com>
2017-03-29 19:13:12 -0400

    hi

--- com.likhon/src/test/java/com/com/likhon/Practice.java
+++ com.likhon/src/test/java/com/com/likhon/Practice.java
@@ -5,6 +5,8 @@ public class Practice {
 	public static void main(String[] args) {
 
 		System.out.println("Hello");
+		FirefoxDriver dr=new FirefoxDriver();
+		dr.get("https://www.bedbathandbeyond.com/");
 	}
 
 }
